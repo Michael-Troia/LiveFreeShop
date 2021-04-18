85ca9ea876dd7a48c5f9171f608433b64024aba9
Michael Troia <troia.michael.a@gmail.com>
2021-04-18 00:05:30 -0400

    feat: create and update inventory items

--- src/main/java/com/example/livefree/Controllers/HomeController.java
+++ src/main/java/com/example/livefree/Controllers/HomeController.java
@@ -1,8 +1,12 @@
 package com.example.livefree.Controllers;
+import com.example.livefree.Models.Item;
 import com.example.livefree.Repositories.ItemRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -16,6 +20,29 @@ public class HomeController {
     @GetMapping("/inventory")
     public String showHome(Model viewModel) {
         viewModel.addAttribute("items", itemRepo.findAll());
+        viewModel.addAttribute("newItem", new Item());
         return "index";
+    }
+
+    @PostMapping("/item/create")
+    public String createItem(@ModelAttribute Item item) {
+        itemRepo.save(item);
+        return "redirect:/inventory";
+    }
+
+    @GetMapping("/item/edit/{id}")
+    public String showEditItem(Model viewModel,
+                               @PathVariable long id){
+        Item item = itemRepo.findById(id);
+        viewModel.addAttribute("item", item);
+        return "item/edit";
+    }
+
+    @PostMapping("/item/edit/{id}")
+    public String editItem(@ModelAttribute Item item,
+                           @PathVariable long id) {
+        item.setId(id);
+        itemRepo.save(item);
+        return "redirect:/inventory";
     }
 }
--- src/main/java/com/example/livefree/Repositories/ItemRepo.java
+++ src/main/java/com/example/livefree/Repositories/ItemRepo.java
@@ -4,5 +4,5 @@ import com.example.livefree.Models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepo extends JpaRepository <Item, Long> {
-
+    Item findById(long id);
 }
